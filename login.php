f39370bc96e6b4f4ca21121075db84b5838e7ea0
Noe993 <noahs.gapna342@gmail.com>
2023-03-09 13:45:44 -0600

    agarre la plantilla

---
+++ login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NOTO | Sign in </title>
+    <link rel="stylesheet" href="css\style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+  </head>
+  <body class="login">
+    
+    <!-- Navbar Start -->
+    <?php
+    include 'navbar.php';
+    ?>
+    <!-- Navbar End -->
+    <div class="container">
+      <div class="wrapper">
+        <div class="title"><span>Login</span></div>
+        <form action="#">
+          <div class="row">
+            <div class="login-img">
+              <img src="img/usuario.gif" alt="">
+            </div>
+            <input type="text" placeholder="Email o Usuario" required>
+          </div>
+          <div class="row">
+            <div class="login-img">
+              <img src="img/lock.png" alt="">
+            </div>
+            <input type="password" placeholder="Contraseña" required>
+          </div>
+          <!-- <div class="pass"><a href="#">Olvidaste la contraseña?</a></div> -->
+          <div class="row button">
+            <input type="submit" value="Login">
+          </div>
+          <div class="signup-link">¿No eres usuario? <a href="sign.php">Registrate</a></div>
+        </form>
+      </div>
+    </div>
+
+  </body>
+</html>
---
+++ sign.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NOTO | Sign in </title>
+    <link rel="stylesheet" href="css\style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+  </head>
+  <body class="login">
+    <div class="container">
+      <div class="wrapper">
+        <div class="title"><span>Registrarse</span></div>
+        <form action="#">
+          <div class="row">
+            <div class="login-img">
+              <img src="img/user.png" alt="">
+            </div>
+            <input type="text" placeholder="Email o Usuario" required>
+          </div>
+          <div class="row">
+            <div class="login-img">
+              <img src="img/lock.png" alt="">
+            </div>
+            <input type="password" placeholder="Contraseña" required>
+            
+          </div>
+          <div class="row">
+            <div class="login-img">
+              <img src="img/lock.png" alt="">
+            </div>
+            <input type="repeat-password" placeholder="Repite la contraseña" required>
+            
+          </div>
+          <!-- <div class="pass"><a href="#">Olvidaste la contraseña?</a></div> -->
+          <div class="row button">
+            <input type="submit" value="Registrarse">
+          </div>
+          <div class="signup-link">¿Ya tienes una cuenta? <a href="login.html">Ingresar</a></div>
+        </form>
+      </div>
+    </div>
+
+  </body>
+</html>
