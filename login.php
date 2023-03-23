0b1276fa27e28e9f1afd9280bbdb95f9216f4793
Jesus Velazquez <jesusduzz@gmail.com>
2023-03-23 16:24:57 -0600

    aas

--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 
-  <body class="login">
+  <body>
     
     <!-- Navbar Start -->
     <?php
@@ -8,33 +8,33 @@
     ?>
     <!-- Navbar End -->
 
-    
+    <div class="login">
 
-    <div class="container">
-      <div class="wrapper">
-        <div class="title"><span>Login</span></div>
-        <form action="procesos/login.php" method="post">
-          <div class="row">
-            <div class="login-img">
-              <img src="img/user.png" alt="">
+      <div class="container">
+        <div class="wrapper">
+          <div class="title"><span>Login</span></div>
+          <form action="procesos/login.php" method="post">
+            <div class="row">
+              <div class="login-img">
+                <img src="img/user.png" alt="">
+              </div>
+              <input type="email" placeholder="Email" required="true" >
             </div>
-            <input type="email" placeholder="Email" required="true" >
-          </div>
-          <div class="row">
-            <div class="login-img">
-              <img src="img/lock.png" alt="">
+            <div class="row">
+              <div class="login-img">
+                <img src="img/lock.png" alt="">
+              </div>
+              <input type="password" name="password" placeholder="Contraseña" required="true">
             </div>
-            <input type="password" name="password" placeholder="Contraseña" required="true">
-          </div>
-          <!-- <div class="pass"><a href="#">Olvidaste la contraseña?</a></div> -->
-          <div class="row button">
-            <input type="submit" value="Login">
-          </div>
-          <div class="signup-link">¿No eres usuario? <a href="sign.php">Registrate</a></div>
-        </form>
+            <!-- <div class="pass"><a href="#">Olvidaste la contraseña?</a></div> -->
+            <div class="row button">
+              <input type="submit" value="Login">
+            </div>
+            <div class="signup-link">¿No eres usuario? <a href="sign.php">Registrate</a></div>
+          </form>
+        </div>
       </div>
     </div>
-
     
   </body>
 </html>
